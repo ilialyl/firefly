6216b81d2c3196a7aa9df0232147b18abd6e4a5d
lyns0 <lynzies.sl@gmail.com>
2025-07-14 17:31:27 +0700

    update player.rs
    add load_track()

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,12 @@
 use color_eyre::eyre::{Ok, Result};
+use rfd::FileDialog;
 use rodio::{Decoder, OutputStream, Sink};
-use std::{fs::File, path::PathBuf};
+use std::{
+    fs::File,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 pub fn get_sink() -> Result<(OutputStream, Sink)> {
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()?;
@@ -16,4 +22,29 @@ pub fn get_source(path: PathBuf) -> Result<Decoder<File>> {
     let source = Decoder::new(file)?;
 
     Ok(source)
+}
+
+pub fn load_track(sink: &Arc<Mutex<Sink>>) -> Option<PathBuf> {
+    let loaded_sink = Arc::clone(sink); // Clone Arc to move into thread
+    let file = FileDialog::new()
+        .add_filter("audio", &["mp3", "flac"])
+        .set_directory("~/")
+        .pick_file();
+
+    let file = match file {
+        Some(f) => f,
+        None => return None,
+    };
+
+    let file_path = Some(file.clone());
+
+    thread::spawn(move || {
+        let source = get_source(file).expect("Error obtaining source");
+
+        let sink = loaded_sink.lock().unwrap();
+        sink.append(source);
+        sink.play();
+    });
+
+    file_path
 }
