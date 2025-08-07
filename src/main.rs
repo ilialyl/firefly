8b2325befc9af07a1cfc08bba19595fe8e3bc631
lyns0 <disse.sl@proton.me>
2025-08-07 15:23:57 +0700

    add remove temp flac file on program exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use color_eyre::eyre::Result;
 
 pub mod player;
@@ -5,8 +7,15 @@ pub mod tui;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
     let mut terminal = ratatui::init();
     let result = tui::App::new().run(&mut terminal);
     ratatui::restore();
+
+    let temp_file = Path::new("temp.flac");
+    if temp_file.exists() {
+        std::fs::remove_file(temp_file).expect("Error removing temporary file.");
+    }
+
     result
 }
