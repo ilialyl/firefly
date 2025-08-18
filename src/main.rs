518e1d851fb93155d1c9de9b372ecd1e2085f160
lyns0 <disse.sl@proton.me>
2025-08-18 16:39:10 +0700

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,39 @@ use std::path::Path;
 
 use color_eyre::eyre::Result;
 
-pub mod app;
+use crate::ui::{
+    message::{Message, handle_events, update},
+    model::{Model, RunningState},
+    view::view,
+};
+
 pub mod player;
 pub mod ui;
+pub mod update;
 
 fn main() -> Result<()> {
+    ui::install_panic_hook();
     color_eyre::install()?;
 
-    let mut terminal = ratatui::init();
-    let result = app::App::new().run(&mut terminal);
-    ratatui::restore();
+    let mut terminal = ui::init_terminal()?;
+    let mut model = Model::default();
+
+    while model.running_state != RunningState::Done {
+        update(&mut model, Message::Tick);
+        terminal.draw(|f| view(&mut model, f))?;
+
+        let mut current_msg = handle_events()?;
+
+        while current_msg.is_some() {
+            current_msg = update(&mut model, current_msg.unwrap());
+        }
+    }
 
     let temp_file = Path::new(player::CONVERTED_TRACK);
     if temp_file.exists() {
         std::fs::remove_file(temp_file).expect("Error removing temporary file.");
     }
 
-    result
+    ui::restore_terminal();
+    Ok(())
 }
