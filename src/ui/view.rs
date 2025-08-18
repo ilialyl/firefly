21dc2a6c29306c52e2f0789c72e3b36b2b7edb97
lyns0 <disse.sl@proton.me>
2025-08-18 16:42:18 +0700

    update ui.rs to view.rs

---
+++ src/ui/view.rs
@@ -0,0 +1,204 @@
+use ratatui::{
+    Frame,
+    layout::{Alignment, Constraint, Direction, Flex, Layout, Rect},
+    style::{Color, Stylize},
+    text::ToSpan,
+    widgets::{Block, Paragraph, Widget},
+};
+
+use crate::{player::Status, ui::model::Model, update};
+
+pub fn view(model: &Model, frame: &mut Frame) {
+    render(model, frame);
+}
+
+pub fn render(model: &Model, frame: &mut Frame) {
+    let outer_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Length(1), Constraint::Fill(1)])
+        .split(frame.area());
+
+    Block::new()
+        .fg(Color::White)
+        .title("Firefly Player".to_span().into_centered_line())
+        .render(outer_layout[0], frame.buffer_mut());
+
+    let inner_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(25), Constraint::Percentage(75)])
+        .split(outer_layout[1]);
+
+    let main_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(60), Constraint::Percentage(40)])
+        .split(inner_layout[1]);
+
+    let left_panel_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(100)])
+        .margin(2)
+        .split(inner_layout[0]);
+
+    frame.render_widget(get_queue_para(model), left_panel_chunks[0]);
+
+    Block::bordered()
+        .fg(Color::White)
+        .title("Player")
+        .title_alignment(Alignment::Right)
+        .render(main_chunks[0], frame.buffer_mut());
+
+    Block::bordered()
+        .fg(Color::White)
+        .title("Control")
+        .title_alignment(Alignment::Right)
+        .render(main_chunks[1], frame.buffer_mut());
+
+    Block::bordered()
+        .fg(Color::White)
+        .title("Queue")
+        .title_alignment(Alignment::Left)
+        .render(inner_layout[0], frame.buffer_mut());
+
+    let player_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(100)])
+        .margin(2)
+        .split(main_chunks[0]);
+
+    draw_player(model, frame, player_chunks[0]);
+
+    let control_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(100)])
+        .margin(2)
+        .horizontal_margin(3)
+        .split(main_chunks[1]);
+
+    draw_controls(frame, control_chunks[0]);
+}
+
+fn draw_player(model: &Model, frame: &mut Frame, chunk: Rect) {
+    let player_text = vec![
+        get_track_name_str(model),
+        "".into(),
+        get_track_pos_str(model),
+        "".into(),
+        get_status_str(model),
+        get_loop_status_str(model),
+        get_info_str(model),
+        get_volume_str(model),
+    ];
+
+    let area = center_vertical(chunk, player_text.len() as u16);
+
+    let player_para = Paragraph::new(player_text.join("\n"))
+        .centered()
+        .alignment(Alignment::Center);
+
+    frame.render_widget(player_para, area);
+}
+
+fn draw_controls(frame: &mut Frame, chunk: Rect) {
+    let controls = vec![
+        " Play/Pause <Space>",
+        " Load Now <N>",
+        " Queue <Q>",
+        " Queue Folder <D>",
+        " Skip <S>",
+        " Rewind/Seek <←/→>",
+        " Volume <↑/↓>",
+        " Loop <L>",
+        " Quit <Esc>",
+    ];
+
+    let rows = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Length(1); 4])
+        .spacing(1)
+        .split(chunk);
+
+    let grid: Vec<Rect> = rows
+        .iter()
+        .flat_map(|col| {
+            Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(vec![Constraint::Percentage(25); 4])
+                .spacing(1)
+                .split(*col)
+                .to_vec()
+        })
+        .collect();
+
+    for (idx, control) in controls.iter().enumerate() {
+        frame.render_widget(Paragraph::new(*control), grid[idx])
+    }
+}
+
+fn get_track_name_str(model: &Model) -> String {
+    match model.track_path.clone() {
+        Some(path) => {
+            if let Some(os_name) = path.file_name() {
+                if let Some(name) = os_name.to_str() {
+                    name.to_string()
+                } else {
+                    "[Invalid UTF-8 name]".into()
+                }
+            } else {
+                "[No file name]".into()
+            }
+        }
+        None => "[Track Empty]".into(),
+    }
+}
+
+fn get_track_pos_str(model: &Model) -> String {
+    update::track_pos_as_str(model)
+}
+
+fn get_status_str(model: &Model) -> String {
+    match model.status {
+        Status::Playing => "Playing".into(),
+        Status::Paused => ("Paused").into(),
+        Status::Idle => ("Idle").into(),
+    }
+}
+
+fn get_loop_status_str(model: &Model) -> String {
+    match model.looping {
+        true => "[Looped]".into(),
+        false => "".into(),
+    }
+}
+
+fn get_volume_str(model: &Model) -> String {
+    format!("Volume: {}%", (model.volume * 100.00).ceil() as i32)
+}
+
+fn get_info_str(model: &Model) -> String {
+    match model.info.last() {
+        Some(str) => str.clone(),
+        None => "".into(),
+    }
+}
+
+fn get_queue_para(model: &Model) -> Paragraph<'static> {
+    let mut track_vec: Vec<String> = Vec::new();
+    for track in model.track_queue.clone() {
+        if let Some(track_name) = track.file_name().unwrap().to_str() {
+            track_vec.push(track_name.to_string());
+        } else {
+            track_vec.push("[Invalid UTF-8 name]".into());
+        }
+    }
+
+    let tracks_str = track_vec.join("\n");
+
+    Paragraph::new(tracks_str)
+}
+
+fn center_vertical(area: Rect, height: u16) -> Rect {
+    let [area] = Layout::vertical([Constraint::Length(height)])
+        .flex(Flex::Center)
+        .areas(area);
+    area
+}
