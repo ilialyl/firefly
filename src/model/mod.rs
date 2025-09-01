fe49227bef5bb876bdace9a79127c276c6cfba99
lyns0 <disse.sl@proton.me>
2025-09-01 13:05:29 +0700

    refactor model fields

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,50 +1,22 @@
-use rodio::{OutputStream, Sink};
 use rust_ffmpeg::FFmpegProcess;
 use std::sync::{Arc, Mutex};
 
-use crate::{
-    logic::player::{self, Track},
-    logic::track_queue::TrackQueue,
-};
-
-#[derive(Debug, Default, PartialEq, Eq)]
-pub enum RunningState {
-    #[default]
-    Running,
-    Busy,
-    Done,
-}
+use crate::logic::{playback_state::PlaybackState, session_state::Session};
 
 pub struct Model {
-    pub running_state: RunningState,
-    pub _stream: OutputStream,
-    pub sink: Sink,
-    pub status: player::Status,
+    pub session: Session,
+    pub playback: PlaybackState,
     pub info_display: String,
-    pub current_track: Track,
-    pub track_queue: TrackQueue,
-    pub looping: bool,
     pub ffmpeg_handle: Option<Arc<Mutex<FFmpegProcess>>>,
 }
 
 impl Default for Model {
     fn default() -> Self {
-        let (stream, sink) = player::get_sink().expect("Error creating sink");
+        let session = Session::new();
         Self {
-            running_state: RunningState::Running,
-            _stream: stream,
-            sink,
-            status: player::Status::Idle,
+            playback: PlaybackState::new(session.get_temp()),
+            session,
             info_display: String::new(),
-            current_track: Track {
-                path: None,
-                pos: None,
-                duration: None,
-                tagged_file: None,
-                has_metadata: false,
-            },
-            track_queue: TrackQueue::new(),
-            looping: false,
             ffmpeg_handle: None,
         }
     }
