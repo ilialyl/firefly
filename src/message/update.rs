9331dfb1408792888e2058a5e74e4199b00246eb
lyns0 <disse.sl@proton.me>
2025-09-02 11:37:23 +0700

    refactor update()
    reorder message match order

--- src/message/update.rs
+++ src/message/update.rs
@@ -21,140 +21,6 @@ pub fn update(
     info_tx: &Sender<String>,
 ) -> (Option<Message>, Result<()>) {
     match msg {
-        Message::LoadNow => {
-            if let Some(path) = player::choose_file() {
-                if let Some(handle) = model.ffmpeg_handle.take() {
-                    let runtime = Runtime::new().unwrap();
-                    runtime.block_on(handle.lock().unwrap().kill()).unwrap();
-                }
-                if let Err(e) = load_now(path, &mut model.playback, msg_tx, info_tx) {
-                    log::error!("{}", e);
-                }
-            }
-
-            (None, Ok(()))
-        }
-        Message::PlayPause => {
-            if model.session.state == RunningState::Busy {
-                return (None, Ok(()));
-            }
-
-            if model.playback.status == PlaybackStatus::Playing {
-                model.playback.sink.pause();
-            } else {
-                model.playback.sink.play();
-            }
-
-            (None, Ok(()))
-        }
-        Message::QueueDir => {
-            if let Some(dir) = player::choose_dir() {
-                model.playback.queue.enqueue_dir(dir);
-            }
-
-            (None, Ok(()))
-        }
-        Message::QueueFile => {
-            if let Some(path_vec) = player::choose_multiple_files() {
-                model.playback.queue.enqueue_tracks(path_vec);
-            }
-
-            (None, Ok(()))
-        }
-        Message::QueueUp => {
-            if let Err(e) = model.playback.queue.move_selected_up() {
-                log::info!("{}", e);
-            };
-
-            (None, Ok(()))
-        }
-
-        Message::QueueDown => {
-            if let Err(e) = model.playback.queue.move_selected_down() {
-                log::info!("{}", e);
-            };
-
-            (None, Ok(()))
-        }
-
-        Message::Quit => {
-            model.session.state = RunningState::Done;
-
-            (None, Ok(()))
-        }
-
-        Message::Rewind => {
-            if model.session.state == RunningState::Busy {
-                return (None, Ok(()));
-            }
-
-            if let Some(track) = model.playback.current.path.clone() {
-                if model.playback.current.path.is_some() {
-                    info_tx.send("Rewinding...".to_string()).unwrap();
-                    if let Err(e) = player::rewind(Duration::from_secs(5), &model.playback) {
-                        let cloned_info_tx = info_tx.clone();
-
-                        log::error!("{}", e);
-                        thread::spawn(move || {
-                            cloned_info_tx.send(e.to_string()).unwrap();
-                            thread::sleep(Duration::from_secs(2));
-                            cloned_info_tx.send("".to_string()).unwrap();
-                        });
-                    };
-                    model.playback.current.duration =
-                        player::read_track_duration(&track, &mut model.playback).ok();
-                    info_tx.send("".to_string()).unwrap();
-                }
-            }
-
-            (None, Ok(()))
-        }
-
-        Message::Seek => {
-            if model.session.state == RunningState::Busy {
-                return (None, Ok(()));
-            }
-
-            if let Some(track_dur) = &model.playback.current.duration {
-                if model.playback.current.path.is_some() {
-                    if let Err(e) =
-                        player::seek(&mut model.playback.sink, track_dur, Duration::from_secs(5))
-                    {
-                        let cloned_info_tx = info_tx.clone();
-
-                        log::error!("{}", e);
-                        thread::spawn(move || {
-                            cloned_info_tx.send(e.to_string()).unwrap();
-                            thread::sleep(Duration::from_secs(2));
-                            cloned_info_tx.send("".to_string()).unwrap();
-                        });
-                    }
-                }
-            }
-
-            (None, Ok(()))
-        }
-
-        Message::Skip => {
-            model.session.state = RunningState::Running;
-
-            if model.playback.queue.is_empty() {
-                return (None, Ok(()));
-            }
-            log::info!("Skipping...");
-
-            if let Some(handle) = model.ffmpeg_handle.take() {
-                let runtime = Runtime::new().unwrap();
-                runtime.block_on(handle.lock().unwrap().kill()).unwrap();
-            }
-
-            if let Err(e) = player::try_next_track(&mut model.playback, msg_tx, info_tx) {
-                log::error!("{}", e);
-            };
-
-            (None, Ok(()))
-        }
-
         Message::Tick => {
             if model.session.state == RunningState::Busy {
                 return (None, Ok(()));
@@ -219,19 +85,139 @@ pub fn update(
 
             (None, Ok(()))
         }
+        Message::LoadNow => {
+            if let Some(path) = player::choose_file() {
+                if let Some(handle) = model.ffmpeg_handle.take() {
+                    let runtime = Runtime::new().unwrap();
+                    runtime.block_on(handle.lock().unwrap().kill()).unwrap();
+                }
+                if let Err(e) = load_now(path, &mut model.playback, msg_tx, info_tx) {
+                    log::error!("{}", e);
+                }
+            }
 
+            (None, Ok(()))
+        }
+        Message::PlayPause => {
+            if model.session.state == RunningState::Busy {
+                return (None, Ok(()));
+            }
+
+            if model.playback.status == PlaybackStatus::Playing {
+                model.playback.sink.pause();
+            } else {
+                model.playback.sink.play();
+            }
+
+            (None, Ok(()))
+        }
+        Message::QueueDir => {
+            if let Some(dir) = player::choose_dir() {
+                model.playback.queue.enqueue_dir(dir);
+            }
+
+            (None, Ok(()))
+        }
+        Message::QueueFile => {
+            if let Some(path_vec) = player::choose_multiple_files() {
+                model.playback.queue.enqueue_tracks(path_vec);
+            }
+
+            (None, Ok(()))
+        }
+        Message::QueueUp => {
+            if let Err(e) = model.playback.queue.move_selected_up() {
+                log::info!("{}", e);
+            };
+
+            (None, Ok(()))
+        }
+        Message::QueueDown => {
+            if let Err(e) = model.playback.queue.move_selected_down() {
+                log::info!("{}", e);
+            };
+
+            (None, Ok(()))
+        }
+        Message::Rewind => {
+            if model.session.state == RunningState::Busy {
+                return (None, Ok(()));
+            }
+
+            if let Some(track) = model.playback.current.path.clone() {
+                if model.playback.current.path.is_some() {
+                    info_tx.send("Rewinding...".to_string()).unwrap();
+                    if let Err(e) = player::rewind(Duration::from_secs(5), &model.playback) {
+                        let cloned_info_tx = info_tx.clone();
+
+                        log::error!("{}", e);
+                        thread::spawn(move || {
+                            cloned_info_tx.send(e.to_string()).unwrap();
+                            thread::sleep(Duration::from_secs(2));
+                            cloned_info_tx.send("".to_string()).unwrap();
+                        });
+                    };
+                    model.playback.current.duration =
+                        player::read_track_duration(&track, &mut model.playback).ok();
+                    info_tx.send("".to_string()).unwrap();
+                }
+            }
+
+            (None, Ok(()))
+        }
+        Message::Seek => {
+            if model.session.state == RunningState::Busy {
+                return (None, Ok(()));
+            }
+
+            if let Some(track_dur) = &model.playback.current.duration {
+                if model.playback.current.path.is_some() {
+                    if let Err(e) =
+                        player::seek(&mut model.playback.sink, track_dur, Duration::from_secs(5))
+                    {
+                        let cloned_info_tx = info_tx.clone();
+
+                        log::error!("{}", e);
+                        thread::spawn(move || {
+                            cloned_info_tx.send(e.to_string()).unwrap();
+                            thread::sleep(Duration::from_secs(2));
+                            cloned_info_tx.send("".to_string()).unwrap();
+                        });
+                    }
+                }
+            }
+
+            (None, Ok(()))
+        }
+        Message::Skip => {
+            model.session.state = RunningState::Running;
+
+            if model.playback.queue.is_empty() {
+                return (None, Ok(()));
+            }
+            log::info!("Skipping...");
+
+            if let Some(handle) = model.ffmpeg_handle.take() {
+                let runtime = Runtime::new().unwrap();
+                runtime.block_on(handle.lock().unwrap().kill()).unwrap();
+            }
+
+            if let Err(e) = player::try_next_track(&mut model.playback, msg_tx, info_tx) {
+                log::error!("{}", e);
+            };
+
+            (None, Ok(()))
+        }
         Message::ToggleArrange => {
             model.playback.queue.toggle_arrange();
 
             (None, Ok(()))
         }
-
         Message::ToggleLoop => {
             model.playback.looping = !model.playback.looping;
 
             (None, Ok(()))
         }
-
         Message::VolumeDown => {
             if model.session.state == RunningState::Busy {
                 return (None, Ok(()));
@@ -241,7 +227,6 @@ pub fn update(
 
             (None, Ok(()))
         }
-
         Message::VolumeUp => {
             if model.session.state == RunningState::Busy {
                 return (None, Ok(()));
@@ -251,19 +236,16 @@ pub fn update(
 
             (None, Ok(()))
         }
-
-        Message::ConversionStarted(handle) => {
-            model.ffmpeg_handle = Some(handle);
-
-            (Some(Message::Busy), Ok(()))
-        }
-
         Message::Busy => {
             model.session.state = RunningState::Busy;
 
             (None, Ok(()))
         }
+        Message::ConversionStarted(handle) => {
+            model.ffmpeg_handle = Some(handle);
 
+            (Some(Message::Busy), Ok(()))
+        }
         Message::ConversionEnded => {
             model.session.state = RunningState::Running;
             if let Some(path) = model.playback.current.path.clone() {
@@ -271,6 +253,11 @@ pub fn update(
                     log::error!("{}", e);
                 };
             }
+
+            (None, Ok(()))
+        }
+        Message::Quit => {
+            model.session.state = RunningState::Done;
 
             (None, Ok(()))
         }
