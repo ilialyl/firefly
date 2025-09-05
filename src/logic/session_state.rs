02f9ee833f06e3f2a5a081b5e2ab5a0b87ba2824
lyns0 <disse.sl@proton.me>
2025-09-05 09:12:57 +0700

    remove Drop implementation for Session

--- src/logic/session_state.rs
+++ src/logic/session_state.rs
@@ -2,8 +2,6 @@ use std::path::PathBuf;
 
 use rand::{Rng, distr::Alphanumeric};
 
-use crate::clean_up;
-
 #[derive(Debug, Default, PartialEq, Eq)]
 pub enum RunningState {
     #[default]
@@ -46,11 +44,5 @@ impl Session {
 
     pub fn get_temp(&self) -> PathBuf {
         PathBuf::from(format!("{}_{}.flac", Self::TEMP_NAME, self.code))
-    }
-}
-
-impl Drop for Session {
-    fn drop(&mut self) {
-        clean_up(self).unwrap();
     }
 }
