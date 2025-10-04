32d6f8982365980a05e237855709f0c258d1529a
lyns0 <disse.sl@proton.me>
2025-10-04 14:41:15 +0700

    update track details view

--- src/player/view/details.rs
+++ src/player/view/details.rs
@@ -4,129 +4,130 @@ use lofty::{
     file::{AudioFile, TaggedFile, TaggedFileExt},
     tag::Accessor,
 };
-use ratatui::{Frame, layout::Rect, widgets::Paragraph};
-
-use crate::{
-    global::view::center_vertical, model::Model, player::logic::playback_status::PlaybackStatus,
+use ratatui::{
+    Frame,
+    layout::{Alignment, Rect},
+    widgets::Paragraph,
 };
+
+use crate::{global::view::center_vertical, model::Model};
 
 pub fn draw(area: Rect, frame: &mut Frame, model: &mut Model) {
     if let Some(current_track) = model.player.current.as_mut()
         && let Some(tagged_file) = current_track.tagged_file.as_mut()
     {
-        let metadata = get_metadata_text_vec(tagged_file);
-        let height = metadata.len() + 1;
+        let metadata = get_metadata_lines(tagged_file);
+        let details = get_file_detail_lines(&current_track.real_path);
         frame.render_widget(
             Paragraph::new(metadata.join("\n")),
-            center_vertical(area, height as u16),
+            center_vertical(area, (metadata.len() + 1) as u16),
+        );
+        frame.render_widget(
+            Paragraph::new(details.join("\n")).alignment(Alignment::Right),
+            center_vertical(area, (details.len() + 1) as u16),
         );
     }
 }
 
-fn get_metadata_text_vec(tag: &TaggedFile) -> Vec<String> {
-    let mut meta_text: Vec<String> = Vec::new();
+fn get_metadata_lines(tagged_file: &TaggedFile) -> Vec<String> {
+    if let Some(tag) = tagged_file.primary_tag() {
+        let properties = tagged_file.properties();
 
-    let track_num = tag
-        .primary_tag()
-        .unwrap()
-        .track()
-        .map(|n| format!("#{} ", n))
-        .unwrap_or("".to_string());
-    let title = tag
-        .primary_tag()
-        .unwrap()
-        .title()
-        .map(|s| format!("{} ", s))
-        .unwrap_or("".to_string());
-    let artist = tag
-        .primary_tag()
-        .unwrap()
-        .artist()
-        .map(|s| format!("{} ", s))
-        .unwrap_or("".to_string());
-    let album = tag
-        .primary_tag()
-        .unwrap()
-        .album()
-        .map(|s| format!("{} ", s))
-        .unwrap_or("".to_string());
-    let year = tag
-        .primary_tag()
-        .unwrap()
-        .year()
-        .map(|n| format!("({}) ", n))
-        .unwrap_or("".to_string());
-    let disc_num = tag
-        .primary_tag()
-        .unwrap()
-        .disk()
-        .map(|n| format!("{} ", n))
-        .unwrap_or("".to_string());
-    let mut bit_depth = tag
-        .properties()
-        .bit_depth()
-        .map(|s| format!("{}-bit/", s))
-        .unwrap_or("".to_string());
+        let track_num = tag
+            .track()
+            .map(|n| format!("#{} ", n))
+            .unwrap_or("".to_string());
+        let title = tag
+            .title()
+            .map(|s| format!("{} ", s))
+            .unwrap_or("No Title".to_string());
+        let artist = tag
+            .artist()
+            .map(|s| format!("{} ", s))
+            .unwrap_or("".to_string());
+        let album = tag
+            .album()
+            .map(|s| format!("{} ", s))
+            .unwrap_or("".to_string());
+        let year = tag
+            .year()
+            .map(|n| format!("({}) ", n))
+            .unwrap_or("".to_string());
+        let disc_num = tag
+            .disk()
+            .map(|n| format!("{} ", n))
+            .unwrap_or("".to_string());
+        let bit_depth = properties
+            .bit_depth()
+            .map(|s| format!("{}-bit/", s))
+            .unwrap_or("16-bit/".to_string());
 
-    if bit_depth.is_empty() {
-        bit_depth = "16-bit/".to_string();
+        let sample_rate = properties
+            .sample_rate()
+            .map(|n| format!("{}kHz ", n / 1000))
+            .unwrap_or("".to_string());
+
+        let bitrate = properties
+            .audio_bitrate()
+            .map(|n| format!("{}kbps ", n))
+            .unwrap_or("".to_string());
+
+        let channels = properties
+            .channels()
+            .map(|n| format!("{} Channels", n))
+            .unwrap_or("".to_string());
+
+        let vec = vec![
+            format!("{}{}", track_num, title),
+            artist.to_string(),
+            format!("{}{}{}", album, disc_num, year),
+            format!("{}{}{}{}", bit_depth, sample_rate, bitrate, channels),
+        ];
+
+        let mut padded_vec = Vec::<String>::new();
+        if let Some((last, rest)) = vec.split_last() {
+            for line in rest {
+                if !line.is_empty() {
+                    padded_vec.push(line.clone());
+                    padded_vec.push(String::new());
+                }
+            }
+            padded_vec.push(last.clone());
+        }
+        padded_vec
+    } else {
+        vec!["Metadata not found.".to_string()]
+    }
+}
+
+fn get_file_detail_lines(path: &Path) -> Vec<String> {
+    let mut vec = Vec::<String>::new();
+    let file_stem = path
+        .file_stem()
+        .and_then(|f| f.to_str())
+        .unwrap_or("No file name")
+        .to_string();
+    let file_ext = path
+        .extension()
+        .and_then(|ext| ext.to_str())
+        .unwrap_or("")
+        .to_string()
+        .to_uppercase();
+
+    vec.push(file_stem);
+    if !file_ext.is_empty() {
+        vec.push(file_ext);
     }
 
-    let sample_rate = tag
-        .properties()
-        .sample_rate()
-        .map(|n| format!("{}kHz ", n / 1000))
-        .unwrap_or("".to_string());
-
-    let bitrate = tag
-        .properties()
-        .audio_bitrate()
-        .map(|n| format!("{}kbps ", n))
-        .unwrap_or("".to_string());
-
-    let lines = [
-        format!("{}{}", track_num, title),
-        artist.to_string(),
-        format!("{}{}{}", album, disc_num, year),
-        format!("{}{}{}", bit_depth, sample_rate, bitrate),
-    ];
-
-    if let Some((last, rest)) = lines.split_last() {
+    let mut padded_vec = Vec::<String>::new();
+    if let Some((last, rest)) = vec.split_last() {
         for line in rest {
             if !line.is_empty() {
-                meta_text.push(line.clone());
-                meta_text.push(String::new());
+                padded_vec.push(line.clone());
+                padded_vec.push(String::new());
             }
         }
-        meta_text.push(last.clone());
+        padded_vec.push(last.clone());
     }
-
-    meta_text
-}
-
-fn get_track_name_str(path: &Path) -> String {
-    if let Some(os_name) = path.file_name() {
-        if let Some(name) = os_name.to_str() {
-            name.to_string()
-        } else {
-            "[Invalid UTF-8 name]".to_string()
-        }
-    } else {
-        "[No file name]".to_string()
-    }
-}
-
-fn get_status_str(player_status: &PlaybackStatus) -> String {
-    match player_status {
-        PlaybackStatus::Playing => "Playing".into(),
-        PlaybackStatus::Paused => ("Paused").into(),
-        PlaybackStatus::Idle => ("Idle").into(),
-    }
-}
-
-fn get_loop_status_str(loop_status: &bool) -> String {
-    match loop_status {
-        true => "[Looped]".into(),
-        false => "".into(),
-    }
+    padded_vec
 }
